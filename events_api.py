7524654234c39719be50d522e55bf980dcaa13b6
nadebu <nadebu@gmail.com>
2023-05-18 05:50:39 +0100

    initial commit

---
+++ events_api.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import datetime
+
+import pandas as pd
+
+UK_COUNTRY_CODE = 97
+
+base_url = "https://images.parkrun.com/events.json"
+response = requests.get(base_url)
+result_json = response.json()
+
+events = []
+
+for feature in result_json["events"]["features"]:
+    id = feature["id"]
+    event_name = feature["properties"]["EventLongName"]
+    country_code = feature["properties"]["countrycode"]
+    long = feature["geometry"]["coordinates"][0]
+    lat = feature["geometry"]["coordinates"][1]
+    event_location = feature["properties"]["EventLocation"]
+
+    output_dict = {
+        "id": id,
+        "name": event_name,
+        "country_code": country_code,
+        "latitude": lat,
+        "longitude": long,
+        "location": event_location,
+    }
+
+    events.append(output_dict)
+
+print(len(events))
+
+df = pd.DataFrame(events)
+
+df["is_junior_event"] = df["name"].str.contains("junior")  # Identifies junior events
+
+df.to_csv("Global events.csv", index=False, header=True)
+
+# UK has a country code of 97
+uk_events = df[df.country_code == UK_COUNTRY_CODE].drop(["country_code"], axis=1)
+uk_events.to_csv("UK Events.csv", index=False, header=True)
